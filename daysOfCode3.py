3339a1730538de7fe138acbd191ff75935e2bfe1
Jules Borusiewicz <jborus27@gmail.com>
2025-12-04 22:14:21 -0500

    cleaned day 3

--- daysOfCode3.py
+++ daysOfCode3.py
@@ -19,41 +19,15 @@ def find_largest_voltage(file):
         final = int(str(num1) + str(num2))
         jolts.append(final)
     return sum(jolts)
-#find_largest_voltage('input3')
-
-##def find_largest_voltage_pt2(file):
-##    fin = open(file)
-##    for line in fin:
-##        num1 = 0
-##        place1 = 0
-##        neww = ""
-##        count = 0
-##        for i in reversed(range(11,-1)):
-##            cur = line[0]
-##            num2 = int(cur)
-##            place2 = 0
-##            for j in range(0,len(fin)-i):
-##                num2 = int(line[i])
-##                cur2 = str(int2)
-##                if cur2 > int2:
-##                    num2 = cur2
-##                    place2 = j
-##            neww += cur2
-##            holder = line[place2+1]
-##            line = holder
-##        count += int(neww)
-
 
 def find_largest_voltage_pt2(file):
     fin = open(file)
     joltages = []
-    #finds the joltage for each line
     for line in fin:
         used = []
         joltages.append(int(find_joltage_helper(0,line, used, "",0)))
     return sum(joltages)
 
-#finds the joltage for a single line
 def find_joltage_helper(iteration, line, used, joltage, largest_index):
     largest = -1
     line = line.strip()
@@ -68,3 +42,4 @@ def find_joltage_helper(iteration, line, used, joltage, largest_index):
     return find_joltage_helper(iteration+1,line, used, joltage,largest_index)
 print(find_largest_voltage_pt2('input3'))
             
+
