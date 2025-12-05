7a66fa449339c7f7c38405f63d0f3b7adc65e939
Jules Borusiewicz <jborus27@gmail.com>
2025-12-04 22:12:05 -0500

    fixed day 3 part 2

--- daysOfCode3.py
+++ daysOfCode3.py
@@ -21,50 +21,50 @@ def find_largest_voltage(file):
     return sum(jolts)
 #find_largest_voltage('input3')
 
+##def find_largest_voltage_pt2(file):
+##    fin = open(file)
+##    for line in fin:
+##        num1 = 0
+##        place1 = 0
+##        neww = ""
+##        count = 0
+##        for i in reversed(range(11,-1)):
+##            cur = line[0]
+##            num2 = int(cur)
+##            place2 = 0
+##            for j in range(0,len(fin)-i):
+##                num2 = int(line[i])
+##                cur2 = str(int2)
+##                if cur2 > int2:
+##                    num2 = cur2
+##                    place2 = j
+##            neww += cur2
+##            holder = line[place2+1]
+##            line = holder
+##        count += int(neww)
+
+
 def find_largest_voltage_pt2(file):
     fin = open(file)
+    joltages = []
+    #finds the joltage for each line
     for line in fin:
-        num1 = 0
-        place1 = 0
-        neww = ""
-        count = 0
-        for i in reversed(range(11,-1)):
-            cur = line[0]
-            num2 = int(cur)
-            place2 = 0
-            for j in range(0,len(fin)-i):
-                num2 = int(line[i])
-                cur2 = str(int2)
-                if cur2 > int2:
-                    num2 = cur2
-                    place2 = j
-            neww += cur2
-            holder = line[place2+1]
-            line = holder
-        count += int(neww)
-print(find_largest_voltage_pt2('inputcheck3.txt'))
+        used = []
+        joltages.append(int(find_joltage_helper(0,line, used, "",0)))
+    return sum(joltages)
 
-##annalee helped with this part lol
-def find_largest_voltage_pt2(file):
-    fin = open(file)
-    for line in fin:
-        num1 = 0
-        place1 = 0
-        neww = ""
-        count = 0
-        for i in reversed(range(11,-1)):
-            cur = line[0]
-            num2 = int(cur)
-            place2 = 0
-            for j in range(0,len(fin)-i):
-                num2 = line[j]
-                cur2 = str(num2)
-                if (cur2 > num2):
-                    num2 = cur2
-                    place2 = j
-            neww += num2;
-            holder = line[place2+1:]
-            line = holder
-        count += int(neww);
-
-                
+#finds the joltage for a single line
+def find_joltage_helper(iteration, line, used, joltage, largest_index):
+    largest = -1
+    line = line.strip()
+    if iteration == 12:
+        return joltage
+    for i in range(largest_index,len(line)-11+iteration):
+        if int(line[i])>int(largest) and i not in used:
+            largest = line[i]
+            largest_index = i
+    used.append(largest_index)
+    joltage += str(largest)
+    return find_joltage_helper(iteration+1,line, used, joltage,largest_index)
+print(find_largest_voltage_pt2('input3'))
+            
